8f99929a41dc581f9610d2542e64ed1d2a0b2149
Nanashi <johnnyjel1234567890@gmail.com>
2023-03-17 11:29:13 -0400

    Update Windows-Activaitor.bat

--- Windows-Activaitor.bat
+++ Windows-Activaitor.bat
@@ -20,17 +20,10 @@ cscript //nologo c:\windows\system32\slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul
 echo ************************************
-set i=1
-:server
-if %i%==1 set KMS_Sev=kms.chinancce.com
-if %i%==2 set KMS_Sev=NextLevel.uk.to
-if %i%==3 set KMS_Sev=GuangPeng.uk.to
-if %i%==4 set KMS_Sev=AlwaysSmile.uk.to
-if %i%==5 set KMS_Sev=kms.chinancce.com 
-if %i%==6 set KMS_Sev=kms.shuax.com
-if %i%==7 exit
+set KMS_Sev=kms.searchforjohn.com
 cscript //nologo c:\windows\system32\slmgr.vbs /skms %KMS_Sev% >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /ato | find /i "successfully"
 echo  ************************************
-choice /n /c YN /m "Do you want to restart your PC now [Y,N]?" & if errorlevel 2 exit) || (echo The connection to the server failed! Trying to connect to another one... & echo Please wait... & set /a i+=1 & goto server)
+choice /n /c YN /m "Do you want to restart your PC now [Y,N]?" || exit
 shutdown.exe /r /t 00
+exit
